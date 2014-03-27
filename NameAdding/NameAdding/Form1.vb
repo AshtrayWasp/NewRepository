2e498bc7d4fc5b0776cca9e1d5e3f1b9de14770e
TheTastyCow <040916399@MY.CENTRAL.WA.EDU.AU>
2014-03-27 11:58:20 +0800

    My changes

--- NameAdding/NameAdding/Form1.vb
+++ NameAdding/NameAdding/Form1.vb
@@ -5,7 +5,12 @@
     End Sub
 
     Private Sub btnAddName_Click(sender As Object, e As EventArgs) Handles btnAddName.Click
-        ListBox1.Items.Add("Joshua")
+        ListBox1.Items.Add("Joshy ;)")
         'ListBox1.Items.Add("Your Name")
+        ListBox1.Items.Add("Thomas Kramer")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
