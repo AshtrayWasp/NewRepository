ec7aebb487a09116e583272feabc93485e306c24
Joshua Danks <041019162@central.wa.edu.au>
2014-03-27 11:33:36 +0800

    poooooooooo

---
+++ NameAdding/NameAdding/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnAddName_Click(sender As Object, e As EventArgs) Handles btnAddName.Click
+        ListBox1.Items.Add("Joshua")
+        'ListBox1.Items.Add("Your Name")
+    End Sub
+End Class
